90af1619fb13e84163bfc73115469c109239a8aa
Pranav0509 <59288046+Pranav0509@users.noreply.github.com>
2025-04-30 11:33:26 +0530

    Update index.js

--- src/components/Final/index.js
+++ src/components/Final/index.js
@@ -8,17 +8,17 @@ class Final extends Component {
       <div className="final-cont">
         <div className='main-cont-5'>
            <div className='big-img-cont'>
-              <img className='big-img' src="https://www.uolo.com/images/award-hori.png"/>
+              <img alt="image" className='big-img' src="https://www.uolo.com/images/award-hori.png"/>
            </div>
            <div className='Uolo-cont'>
                <h1>Uolo in media</h1>
            </div>
            <div className='imgs-cont'>
-               <img className='comp-img' src="https://www.uolo.com/images/tech_crunch.svg"/>
-               <img className='comp-img' src="https://www.uolo.com/images/Logo_Inc%2042.svg"/>
-               <img className='comp-img' src="https://www.uolo.com/images/entrackr.svg"/>
-               <img className='comp-img' src="https://www.uolo.com/images/Logo_ET.svg" />
-               <img className='comp-img' src="https://www.uolo.com/images/Logo_Financial_express.svg"/>
+               <img alt="image" className='comp-img' src="https://www.uolo.com/images/tech_crunch.svg"/>
+               <img alt="image" className='comp-img' src="https://www.uolo.com/images/Logo_Inc%2042.svg"/>
+               <img alt="image" className='comp-img' src="https://www.uolo.com/images/entrackr.svg"/>
+               <img alt="image" className='comp-img' src="https://www.uolo.com/images/Logo_ET.svg" />
+               <img alt="image" className='comp-img' src="https://www.uolo.com/images/Logo_Financial_express.svg"/>
            </div>
         </div>
         <div className="banner-cont">
@@ -33,10 +33,12 @@ class Final extends Component {
             <img
               className="app-img"
               src="https://www.uolo.com/images/google-play-store-badge.png"
+                alt="image"
             />
             <img
               className="app-img"
               src="https://www.uolo.com/images/apple-app-store-badge.png"
+                alt="image"
             />
           </div>
           <div className="col">
